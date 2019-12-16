1f63e927fa88401d70f1c1e054823787e14d3aa6
yusijia <yusijia0501@126.com>
2019-12-16 08:33:42 +0800

    [ADD] 1.增加反转链表

---
+++ src/main/java/com/chaexsy/practise/leetcode/linkedlist/exercises/InvertedLinkList.java
@@ -0,0 +1,26 @@
+package com.chaexsy.practise.leetcode.linkedlist.exercises;
+
+/**
+ * 反转链表
+ *
+ * 反转一个单链表
+ * @author Chaexsy 2019-12-16 8:31
+ */
+public class InvertedLinkList {
+      class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        while (head.next != null){
+            //
+        }
+
+        return null;
+    }
+}
--- src/main/java/com/chaexsy/practise/leetcode/linkedlist/exercises/RemoveNodeFromEnd.java
+++ src/main/java/com/chaexsy/practise/leetcode/linkedlist/exercises/RemoveNodeFromEnd.java
@@ -58,6 +58,31 @@ public class RemoveNodeFromEnd {
         }
     }
 
+    /**
+     * 第一个指针从列表的开头向前移动 n+1步，而第二个指针将从列表的开头出发。
+     * 现在，这两个指针被 n个结点分开。
+     * 我们通过同时移动两个指针向前来保持这个恒定的间隔，直到第一个指针到达最后一个结点。
+     * 此时第二个指针将指向从最后一个结点数起的第 n个结点。
+     * 我们重新链接第二个指针所引用的结点的 next 指针指向该结点的下下个结点。
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
     public static void main(String[] args){
 //        ListNode head = new ListNode(1);
 //        head.next = new ListNode(2);
