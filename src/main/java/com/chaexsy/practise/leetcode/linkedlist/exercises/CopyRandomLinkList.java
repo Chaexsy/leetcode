571ff7fad6deb1fca10615ab1887e531e0ac5103
yusijia <yusijia0501@126.com>
2020-01-03 08:36:30 +0800

    增加拷贝随机链表的题解

---
+++ src/main/java/com/chaexsy/practise/leetcode/linkedlist/exercises/CopyRandomLinkList.java
@@ -0,0 +1,76 @@
+package com.chaexsy.practise.leetcode.linkedlist.exercises;
+
+import java.util.HashMap;
+
+/**
+ * 复制带随机指针的链表
+ *
+ * 给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+ * 要求返回这个链表的深拷贝。
+ *
+ * @author Chaexsy 2020-01-03 8:20
+ */
+public class CopyRandomLinkList {
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+
+        public Node(int val, Node next, Node random) {
+            this.val = val;
+            this.next = next;
+            this.random = random;
+        }
+    }
+
+    /**
+     * Visited dictionary to hold old node reference as "key" and new node reference as the "value"
+     */
+    private HashMap<Node, Node> visited = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node oldNode = head;
+
+        // Creating the new head node.
+        Node newNode = new Node(oldNode.val);
+        this.visited.put(oldNode, newNode);
+
+        // Iterate on the linked list until all nodes are cloned.
+        while (oldNode != null) {
+            // Get the clones of the nodes referenced by random and next pointers.
+            newNode.random = this.getClonedNode(oldNode.random);
+            newNode.next = this.getClonedNode(oldNode.next);
+
+            // Move one step ahead in the linked list.
+            oldNode = oldNode.next;
+            newNode = newNode.next;
+        }
+        return this.visited.get(head);
+    }
+
+    public Node getClonedNode(Node node) {
+        // If the node exists then
+        if (node != null) {
+            // Check if the node is in the visited dictionary
+            if (this.visited.containsKey(node)) {
+                // If its in the visited dictionary then return the new node reference from the dictionary
+                return this.visited.get(node);
+            } else {
+                // Otherwise create a new node, add to the dictionary and return it
+                this.visited.put(node, new Node(node.val, null, null));
+                return this.visited.get(node);
+            }
+        }
+        return null;
+    }
+}
