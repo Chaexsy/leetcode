a6d3b7f0e14079e9f237ef14b1138582a6be3a9f
yusijia <yusijia0501@126.com>
2020-01-27 22:50:38 +0800

    1.二叉树的前序遍历 2.打印工具类增加一个打印List的方法

--- src/main/java/com/chaexsy/practise/leetcode/binarytree/InorderTraversal.java
+++ src/main/java/com/chaexsy/practise/leetcode/binarytree/InorderTraversal.java
@@ -3,15 +3,15 @@ package com.chaexsy.practise.leetcode.binarytree;
 import com.chaexsy.practise.leetcode.utils.PrintUtil;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 /**
- * 二叉树的前序遍历
- * 给定一个二叉树，返回它的 前序 遍历。
+ * 中序遍历二叉树
  *
- *  示例:
+ * 给定一个二叉树，返回它的中序 遍历。
+ *
+ * 示例:
  * 输入: [1,null,2,3]
  *    1
  *     \
@@ -19,14 +19,14 @@ import java.util.Stack;
  *     /
  *    3
  *
- * 输出: [1,2,3]
+ * 输出: [1,3,2]
  * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  *
- * @author Chaexsy 2020-01-27 15:28
+ * @author Chaexsy 2020-01-27 18:14
  */
-public class PreorderTraversal {
+public class InorderTraversal {
     /**
-     * 解法1：
+     * 解法1
      * 递归，深度优先搜索
      */
     public List<Integer> solution(TreeNode root) {
@@ -36,44 +36,43 @@ public class PreorderTraversal {
     }
 
     private void ldr(TreeNode node, List<Integer> list){
-        if (node == null) return;
-        list.add(node.val);
+        if(node == null){
+            return;
+        }
+
         ldr(node.left, list);
+        list.add(node.val);
         ldr(node.right, list);
     }
 
     /**
-     * 解法2：
-     * 迭代，利用栈后进先出特性
+     * 解法2
+     * 迭代
      */
     public List<Integer> solution2(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
 
         Stack<TreeNode> stack = new Stack<>();
-        stack.add(root);
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            result.add(node.val);
-            if (node.right != null) {
-                stack.add(node.right);
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
             }
-            if (node.left != null) {
-                stack.add(node.left);
-            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
         }
         return result;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(5);
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(4);
 
-        PrintUtil.printList(new PreorderTraversal().solution(root));
+        PrintUtil.printList(new InorderTraversal().solution2(root));
     }
 }
--- src/main/java/com/chaexsy/practise/leetcode/binarytree/PostorderTraversal.java
+++ src/main/java/com/chaexsy/practise/leetcode/binarytree/PostorderTraversal.java
@@ -8,10 +8,11 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 二叉树的前序遍历
- * 给定一个二叉树，返回它的 前序 遍历。
+ * 二叉树的后序遍历
  *
- *  示例:
+ * 给定一个二叉树，返回它的 后序 遍历。
+ * 示例:
+ *
  * 输入: [1,null,2,3]
  *    1
  *     \
@@ -19,14 +20,14 @@ import java.util.Stack;
  *     /
  *    3
  *
- * 输出: [1,2,3]
+ * 输出: [3,2,1]
  * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  *
- * @author Chaexsy 2020-01-27 15:28
+ * @author Chaexsy 2020-01-27 20:14
  */
-public class PreorderTraversal {
+public class PostorderTraversal {
     /**
-     * 解法1：
+     * 解法1
      * 递归，深度优先搜索
      */
     public List<Integer> solution(TreeNode root) {
@@ -36,18 +37,20 @@ public class PreorderTraversal {
     }
 
     private void ldr(TreeNode node, List<Integer> list){
-        if (node == null) return;
-        list.add(node.val);
+        if(node == null){
+            return;
+        }
         ldr(node.left, list);
         ldr(node.right, list);
+        list.add(node.val);
     }
 
     /**
-     * 解法2：
-     * 迭代，利用栈后进先出特性
+     * 解法2
+     * 迭代
      */
     public List<Integer> solution2(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
+        LinkedList<Integer> result = new LinkedList<>();
         if (root == null) {
             return result;
         }
@@ -56,24 +59,24 @@ public class PreorderTraversal {
         stack.add(root);
         while (!stack.isEmpty()) {
             TreeNode node = stack.pop();
-            result.add(node.val);
-            if (node.right != null) {
-                stack.add(node.right);
-            }
+            result.addFirst(node.val);
             if (node.left != null) {
                 stack.add(node.left);
+            }
+            if (node.right != null) {
+                stack.add(node.right);
             }
         }
         return result;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(5);
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(4);
 
-        PrintUtil.printList(new PreorderTraversal().solution(root));
+        PrintUtil.printList(new PostorderTraversal().solution2(root));
     }
 }
